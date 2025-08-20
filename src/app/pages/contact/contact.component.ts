55fb88d400f529c8c0dfd2b1527cab5c78717750
Lebohang <monamane.lebohang45@gmail.com>
2025-08-20 13:16:47 +0200

    Done

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,6 +1,6 @@
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
 import emailjs from 'emailjs-com';
 
 @Component({
@@ -8,60 +8,40 @@ import emailjs from 'emailjs-com';
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss'],
   standalone: true,
-  imports: [
-    CommonModule,
-    FormsModule
-  ]
+  imports: [CommonModule, FormsModule]
 })
 export class ContactComponent {
-  messageStatus: string = '';
+  messageStatus = '';
 
-  // Bindable form fields
-  user_name: string = '';
-  user_phone: string = '';
-  user_email: string = '';
-  user_address: string = '';
-  message: string = '';
+  user_name = '';
+  user_phone = '';
+  user_email = '';
+  user_address = '';
+  message = '';
 
-  sendEmail(e: Event) {
-    e.preventDefault();
+  @ViewChild('contactFormElement') contactFormElement!: ElementRef<HTMLFormElement>;
 
-    const form = e.target as HTMLFormElement;
-
-    // Add current time to the form
-    const timeField = document.createElement('input');
-    timeField.type  = 'hidden';
-    timeField.name  = 'time';
-    timeField.value = new Date().toLocaleString();
-    form.appendChild(timeField);
-
-    // Force values into the form fields
-    (form.querySelector('input[name="user_name"]')  as HTMLInputElement).value = this.user_name;
-    (form.querySelector('input[name="user_phone"]') as HTMLInputElement).value = this.user_phone;
-    (form.querySelector('input[name="user_email"]') as HTMLInputElement).value = this.user_email;
-    (form.querySelector('input[name="user_address"]') as HTMLInputElement).value = this.user_address;
-    (form.querySelector('textarea[name="message"]') as HTMLTextAreaElement).value = this.message;
+  sendEmail(form: NgForm) {
+    if (form.invalid) {
+      Object.values(form.controls).forEach(control => control.markAsTouched());
+      return;
+    }
 
     emailjs.sendForm(
       'service_appistq',
       'template_bl5swzd',
-      form,
+      this.contactFormElement.nativeElement, 
       'IGbiyBQ3uFd8ArL5p'
     ).then(() => {
       this.messageStatus = 'Message sent successfully!';
-
-      // Reset **after** a short delay so EmailJS reads correct values
-      setTimeout(() => {
-        form.reset();
-        this.user_name    = '';
-        this.user_phone   = '';
-        this.user_email   = '';
-        this.user_address = '';
-        this.message      = '';
-        this.messageStatus = '';
-      }, 1500);
-
-    }, () => {
+      form.resetForm();
+      this.user_name = '';
+      this.user_phone = '';
+      this.user_email = '';
+      this.user_address = '';
+      this.message = '';
+      setTimeout(() => this.messageStatus = '', 1500);
+    }).catch(() => {
       this.messageStatus = 'Oops! Something went wrong. Please try again.';
       setTimeout(() => this.messageStatus = '', 8000);
     });
